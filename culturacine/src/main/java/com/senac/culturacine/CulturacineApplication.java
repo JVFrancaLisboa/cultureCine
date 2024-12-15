ae7e412cfeee4049449e7d3c7498c639aa474420
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-14 22:45:20 -0300

    first commit

---
+++ culturacine/src/main/java/com/senac/culturacine/CulturacineApplication.java
@@ -0,0 +1,13 @@
+package com.senac.culturacine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturacineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturacineApplication.class, args);
+	}
+
+}
---
+++ culturacine/src/test/java/com/senac/culturacine/CulturacineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.culturacine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturacineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
