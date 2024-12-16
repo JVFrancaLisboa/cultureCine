1970a5745f57640745c94ad793a723d14b680f1e
SyntaxxSeeker <logiccoder88@gmail.com>
2024-12-15 21:40:28 -0300

    Created the first test to verify saving in FilmeRepository

--- culturacine/src/test/java/com/senac/culturacine/repository/FilmeRepositoryTest.java
+++ culturacine/src/test/java/com/senac/culturacine/repository/FilmeRepositoryTest.java
@@ -1,10 +1,16 @@
 package com.senac.culturacine.repository;
 
+import com.senac.culturacine.entities.AnaliseEntity;
+import com.senac.culturacine.entities.FilmeEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -21,7 +27,18 @@ class FilmeRepositoryTest {
 
     @Test
     void TestarSalvarFilme(){
-        String p = "p";
-        assertThat(p).isEqualTo("p");
+        List<AnaliseEntity> analises = new ArrayList<>();
+        FilmeEntity filme = new FilmeEntity();
+        filme.setTitulo("Matrix");
+        filme.setSinopse("Um clássico...");
+        filme.setAno("2000");
+        filme.setGenero("Suspense");
+        filme.setAnalises(analises);
+
+        FilmeEntity salvo = filmeRepository.save(filme);
+
+        assertThat(salvo).isNotNull();
+        assertThat(salvo.getId()).isNotNull();
+        assertThat(salvo.getTitulo()).isEqualTo("Matrix");
     }
 }
